76b3356f00b88dfb39a8e36adae73316d82d28b5
刘畅 <736779328@qq.com>
2022-10-16 11:02:43 +0800

    init

---
+++ hello/src/bin/main.rs
@@ -0,0 +1,48 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
+
+use hello::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(2);
+
+    listener.incoming().for_each(|result| {
+        let stream = result.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    });
+    println!("Shutting down.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read_exact(&mut buffer).unwrap();
+    // println!("Request:{}", String::from_utf8_lossy(&buffer[..]));
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+    stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,8 +1,10 @@
+use std::sync::mpsc;
+use std::thread;
 use std::time::Duration;
 
 use async_std::task;
 
-#[async_std::main]
+/*#[async_std::main]
 async fn main() {
     /*    thread::spawn(move || {
         for i in 0..10 {
@@ -19,9 +21,34 @@ async fn main() {
     let do3_async = task::spawn(do3());
     do4().await;
     do3_async.await;
+}*/
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("{}", received);
+
+    let x = 1;
+    if let 1..=5 = x {
+        println!("test");
+    }
+
+    let num = Some(3);
+    match num {
+        Some(x @ 0..=2) if x < 5 => {
+            println!("{x}")
+        }
+        None => {}
+        _ => {}
+    }
 }
 
-async fn do3() {
+/*async fn do3() {
     for i in 0..5 {
         println!("do3:{}", i);
         task::sleep(Duration::from_secs(1)).await;
@@ -33,4 +60,4 @@ async fn do4() {
         println!("do4:{}", i);
         task::sleep(Duration::from_secs(1)).await;
     }
-}
+}*/
